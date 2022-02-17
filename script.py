39c07797bf48ddcf9e84cc93a444ade2e1d26d95
mtr <matthewreilly841@gmail.com>
2022-02-17 11:41:24 -0500

    dynamic thread number

--- script.py
+++ script.py
@@ -1,75 +1,55 @@
 import requests
-import threading
+from threading import Thread
 import sys
 
+# Initialize output file
+output = open("tokenIds.txt", "w")
+
+# Read in cli arguments
+url = sys.argv[1]
+collection_size = int(sys.argv[2])
+n_threads = int(sys.argv[3])
+output_arg = sys.argv[4]
 print(sys.argv)
 
-url = sys.argv[1]
-output_arg = sys.argv[2]
-n_threads = sys.argv[3]
+# Calculate the bound size or range for thread argument loop
+bound_size = collection_size // n_threads
 
 
-def task1():
-    thread1_loc = 0
-    while thread1_loc <= 1500:
-        res = requests.get(url + str(thread1_loc)).json()
-        print(res["attributes"][2], res[output_arg])
-        thread1_loc += 1
+def task(location, end):
+    while location <= end:
+        res = requests.get(url + str(location)).json()
+        output.write(str(res[output_arg]) + "\n")
+        print(res[output_arg])
+        location += 1
 
-def task2():
-    thread2_loc = 1501
-    while thread2_loc < 3000:
-        res = requests.get(url + str(thread2_loc)).json()
-        print(res["attributes"][2], res["id"])
-        thread2_loc += 1
 
-def task3():
-    thread3_loc = 3001
-    while thread3_loc < 4500:
-        res = requests.get(url + str(thread3_loc)).json()
-        print(res["attributes"][2], res["id"])
-        thread3_loc += 1
+def getBounds(n):
+    res = [0] * 2
+    # res[0] = previous end
+    # res[1] = new end
+    if n == 0:
+        res[0] = 0
+        res[1] = bound_size - 1
+    elif n + 1 == n_threads:
+        res[0] = bound_size * n
+        res[1] = collection_size - 1
+    else:
+        res[0] = bound_size * n
+        res[1] = bound_size * (n + 1) - 1
 
-def task4():
-    thread4_loc = 4501
-    while thread4_loc < 6000:
-        res = requests.get(url + str(thread4_loc)).json()
-        print(res["attributes"][2], res["id"])
-        thread4_loc += 1
+    print(res)
+    return res
 
-def task5():
-    thread5_loc = 6001
-    while thread5_loc < 7500:
-        res = requests.get(url + str(thread5_loc)).json()
-        print(res["attributes"][2], res["id"])
-        thread5_loc += 1
 
-def task6():
-    thread6_loc = 7501
-    while thread6_loc < 9599:
-        res = requests.get(url + str(thread6_loc)).json()
-        print(res["attributes"][2], res["id"])
-        thread6_loc += 1
+threads = []
+for n in range(n_threads):
+    bounds = getBounds(n)
+    t = Thread(target=task, args=(bounds[0], bounds[1]))
+    t.start()
+    threads.append(t)
 
-t1 = threading.Thread(target=task1, name='t1')
-t2 = threading.Thread(target=task2, name='t2')  
-t3 = threading.Thread(target=task3, name='t3')  
-t4 = threading.Thread(target=task4, name='t4')
-t5 = threading.Thread(target=task5, name='t5')  
-t6 = threading.Thread(target=task6, name='t6')
+for t in threads:
+    t.join()
 
-# starting threads
-t1.start()
-t2.start()
-t3.start()
-t4.start()
-t5.start()
-t6.start()
-  
-# wait until all threads finish
-t1.join()
-t2.join()
-t3.join()
-t4.join()
-t5.join()
-t6.join()
+output.close()
